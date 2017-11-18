f5c384b77af9fe3ec8400c94993985f2d73d6bd5
alfredo-PC\alfredo <alfredovaldes@uadec.edu.mx>
2017-11-17 18:57:14 -0600

    ya jala xd

--- Script.py
+++ Script.py
@@ -1,14 +1,20 @@
+#-*- coding: utf-8 -*-
+import os
+import time
+import threading
+import subprocess
 import urllib.request
 import urllib.error
-import json
+import json as json
 import mimetypes
+from gps3 import gps3
 
 
-def signin(email, password):
+def signin():
     global refreshToken
     my_data = dict()
-    my_data["email"] = email
-    my_data["password"] = password
+    my_data["email"] = "admin@miag5.uadec.mx"
+    my_data["password"] = "proyectoMIAg5"
     my_data["returnSecureToken"] = True
 
     json_data = json.dumps(my_data).encode()
@@ -19,12 +25,12 @@ def signin(email, password):
     try:
         loader = urllib.request.urlopen(request)
     except urllib.error.URLError as e:
-        message = json.loads(e.read())
+        message = json.loads(e.read().decode("utf-8"))
         print(message["error"]["message"])
     else:
-        data = json.loads(loader.read())
+        data = json.loads(loader.read().decode("utf-8"))
         refreshToken = data['refreshToken']
-        # print(data['idToken'])
+        print(data)
 
 
 def refresh_accesstoken(token):
@@ -42,11 +48,12 @@ def refresh_accesstoken(token):
     try:
         loader = urllib.request.urlopen(request)
     except urllib.error.URLError as e:
-        message = json.loads(e.read())
+        message = json.loads(e.read().decode("utf-8"))
         print(message["error"]["message"])
     else:
-        data = json.loads(loader.read())
+        data = json.loads(loader.read().decode("utf-8"))
         access_token = data['access_token']
+        print(access_token)
 
 
 def upload_file():
@@ -68,17 +75,62 @@ def upload_file():
     try:
         loader = urllib.request.urlopen(my_request)
     except urllib.error.URLError as e:
-        message = json.loads(e.read())
+        message = json.loads(e.read().decode("utf-8"))
         print(message["error"]["message"])
     else:
-        print(loader.read())
+        print(loader.read().decode("utf-8"))
 
 
+def save_entry(lat,lon,app_id,numCamion,access_token):
+    my_data = dict()
+    my_data["lat"] = lat
+    my_data["lon"] = lon
+    my_data["timestamp"] = {".sv": "timestamp"}
+
+    json_data = json.dumps(my_data).encode()
+    print("https://"+app_id+".firebaseio.com/camiones/"+numCamion+".json")
+    try:
+        loader = urllib.request.urlopen("https://"+app_id+".firebaseio.com/camiones/"+numCamion+".json?auth="+access_token, data=json_data)
+        print("https://"+app_id+".firebaseio.com/camiones/"+numCamion)
+    except urllib.error.URLError as e:
+        message = json.loads(e.read().decode("utf-8"))
+        print(message["error"])
+    else:
+        print(loader.read().decode("utf-8"))
+
+def takePicture():
+    bashCommand = "fswebcam -d /dev/video0 --set brightness=50% foto_"+numCamion+".png"
+    output = subprocess.check_output(['bash','-c', bashCommand])
+    time.sleep(3)
+
+    
+def gpsPoll(lat,lon):
+    conexion = gps3.GPSDSocket()
+    stream = gps3.DataStream()
+    conexion.connect()
+    conexion.watch()
+    for datos in conexion:
+        if datos:
+            stream.unpack(datos)
+            print(stream.TPV['time'])
+            lat = stream.TPV['lat']
+            lon = stream.TPV['lon']
+            print(lat,"\n",lon,"\n")
+            takePicture()
+            save_entry(lat,lon,app_id,numCamion,access_token)
+            upload_file()
+
+
+    
 refreshToken = 0
 access_token = 0
-app_id = "app_id"
-filename = "image.png"
-firebase_apikey = "apikey"
-signin("email", "password")
-refresh_accesstoken(refreshToken)
-upload_file()
+lat = " "
+lon = " "
+numCamion = "2"
+app_id = "testfire-miag5"
+filename = "foto_"+numCamion+".png"
+firebase_apikey = "AIzaSyA-TEtL45EZDYWF28aNRfWzXXG6-yJaxlM"
+signin()
+while True:
+    refresh_accesstoken(refreshToken)
+    gpsPoll(lat,lon)
